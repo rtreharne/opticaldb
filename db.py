db3248e1e161612da53263c3b945d5dbb0c19cbb
Robert <R.Treharne@liverpool.ac.uk>
2015-11-16 17:13:57 +0000

    Added shelves, shelf functions to db.py

---
+++ db.py
@@ -0,0 +1,44 @@
+import yaml
+from tabulate import tabulate
+
+class Library:
+    
+    def read_data(self):
+        with open('database/new.yml', 'r') as f:
+            doc = yaml.load(f)
+
+        return doc
+
+    def shelves(self):
+
+        table=[]
+        doc = self.read_data()    
+        for item in doc:
+            table.append([item['SHELF'], item['name']])
+
+        print tabulate(table,
+                       headers=['SHELF', 'DESCRIPTION'],
+                       tablefmt='orgtbl')
+
+    def shelf(self, key):
+        count = 0
+        table=[]
+        a = self.read_data()
+        for item in a:
+            if item['SHELF']==key:
+                for book in item['content']:
+                    if 'BOOK' in book:
+                        table.append([book['BOOK'], book['name']])
+                    else:
+                        continue
+                break
+            count+=1
+
+        print tabulate(table,
+                       headers=['BOOK', 'DESCRIPTION'],
+                       tablefmt='orgtbl')
+
+    def book(self, key)
+
+        
+            
